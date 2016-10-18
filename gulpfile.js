ee3dd4786731ac2bf2a7345813bc0abca7e8bcae
Jem Rayfield <jem.rayfield@ft.com>
2016-10-18 19:05:46 +0100

    Fix up deploy to use cloudformation

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,6 @@ var cloudformation = require('gulp-cloudformation');
 const template = require('gulp-template');
 var config = { useIAM: true };
 var s3 = require('gulp-s3-upload')(config);
-
-
 var paths = {};
 
 paths.src = {};
@@ -100,7 +98,8 @@ gulp.task('deploy:cloudformation-template', function() {
     gulp.src('cloudformation/flex-cloudformation-brightcove.json')
         .pipe(template({lambdarole: gutil.env.lambdarole,
                         account: gutil.env.account,
-                        accountforrole: gutil.env.account}))
+                        accountforrole: gutil.env.account,
+                        environment: gutil.env.env}))
         .pipe(gulp.dest('cloudformation/' + gutil.env.env));
 });
 
--- index.js
+++ index.js
@@ -24,6 +24,8 @@ exports.handler = function(event, context) {
     docClient.query(params, function(err, data) {
         if (err) {
             console.error("Unable to query for entity. Error JSON:", JSON.stringify(err, null, 2));
+            var errorString = JSON.stringify(err, null, 2).trim().replace(/\r?\n|\r/g, " ");
+            context.fail(errorString)
         } else {
             console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
             context.succeed(data);
